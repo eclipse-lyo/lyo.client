f3e9a7644433c7e8fa70010d8c79b095a27ba19a
Benjamin Röhl <benjamin.roehl@pure-systems.com>
2018-05-15 10:33:41 +0200

    Bug 528403 - Architecture Management Resource not compliant to AM 2.0
    Change-Id: Ie65583a3823fee8a45bebb9f1b0204f5e434eb48
    Signed-off-by: Benjamin Röhl <benjamin.roehl@pure-systems.com>

--- org.eclipse.lyo.client.java/src/main/java/org/eclipse/lyo/client/oslc/resources/ArchitectureResource.java
+++ org.eclipse.lyo.client.java/src/main/java/org/eclipse/lyo/client/oslc/resources/ArchitectureResource.java
@@ -18,6 +18,7 @@ package org.eclipse.lyo.client.oslc.resources;
 import java.net.URI;
 import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
 
@@ -45,252 +46,254 @@ import org.eclipse.lyo.oslc4j.core.model.ValueType;
 public final class ArchitectureResource
 extends AbstractResource
 {
-	private final Set<URI>      contributors                = new TreeSet<URI>();
-    private final Set<URI>      creators                    = new TreeSet<URI>();
-    private final Set<String>   dctermsTypes                = new TreeSet<String>();
-    private final Set<URI>      rdfTypes                    = new TreeSet<URI>();
+  private final Set<URI>    contributors = new TreeSet<URI>();
+  private final Set<URI>    creators     = new TreeSet<URI>();
+  private final Set<String> dctermsTypes = new TreeSet<String>();
+  private final Set<URI>    rdfTypes     = new TreeSet<URI>();
 
-
-    private Date     created;
-    private String   description;
-    private String   identifier;
-    private URI      source;
-    private URI      instanceShape;
-    private Date     modified;
-    private URI      serviceProvider;
-    private String   title;
+  private Date              created;
+  private String            description;
+  private String            identifier;
+  private URI               source;
+  private URI               instanceShape;
+  private Date              modified;
+  private Set<URI>          serviceProviders;
+  private String            title;
 
 	public ArchitectureResource()
 	{
-		super();
+    super();
 
-		rdfTypes.add(URI.create(ArchitectureConstants.TYPE_ARCHITECTURE_RESOURCE));
-	}
+    rdfTypes.add(URI.create(ArchitectureConstants.TYPE_ARCHITECTURE_RESOURCE));
+  }
 
     public ArchitectureResource(final URI about)
      {
-         super(about);
+    super(about);
 
-		rdfTypes.add(URI.create(ArchitectureConstants.TYPE_ARCHITECTURE_RESOURCE));
-     }
+    rdfTypes.add(URI.create(ArchitectureConstants.TYPE_ARCHITECTURE_RESOURCE));
+  }
 
-    protected URI getRdfType() {
-    	return URI.create(ArchitectureConstants.TYPE_ARCHITECTURE_RESOURCE);
-    }
+  protected URI getRdfType() {
+    return URI.create(ArchitectureConstants.TYPE_ARCHITECTURE_RESOURCE);
+  }
 
     public void addContributor(final URI contributor)
     {
-        this.contributors.add(contributor);
-    }
+    this.contributors.add(contributor);
+  }
 
     public void addCreator(final URI creator)
     {
-        this.creators.add(creator);
-    }
+    this.creators.add(creator);
+  }
 
     public void addRdfType(final URI rdfType)
     {
-        this.rdfTypes.add(rdfType);
-    }
+    this.rdfTypes.add(rdfType);
+  }
 
     public void addDctermsType(final String dctermsType)
     {
-        this.dctermsTypes.add(dctermsType);
-    }
+    this.dctermsTypes.add(dctermsType);
+  }
 
-
-    @OslcDescription("The person(s) who are responsible for the work needed to complete the automation plan.")
-    @OslcName("contributor")
-    @OslcPropertyDefinition(OslcConstants.DCTERMS_NAMESPACE + "contributor")
-    @OslcRange(QmConstants.TYPE_PERSON)
-    @OslcTitle("Contributors")
+  @OslcDescription("The person(s) who are responsible for the work needed to complete the automation plan.")
+  @OslcName("contributor")
+  @OslcPropertyDefinition(OslcConstants.DCTERMS_NAMESPACE + "contributor")
+  @OslcRange(QmConstants.TYPE_PERSON)
+  @OslcTitle("Contributors")
     public URI[] getContributors()
     {
-        return contributors.toArray(new URI[contributors.size()]);
-    }
+    return contributors.toArray(new URI[contributors.size()]);
+  }
 
-    @OslcDescription("Timestamp of resource creation.")
-    @OslcPropertyDefinition(OslcConstants.DCTERMS_NAMESPACE + "created")
-    @OslcReadOnly
-    @OslcTitle("Created")
+  @OslcDescription("Timestamp of resource creation.")
+  @OslcPropertyDefinition(OslcConstants.DCTERMS_NAMESPACE + "created")
+  @OslcReadOnly
+  @OslcTitle("Created")
     public Date getCreated()
     {
-        return created;
-    }
+    return created;
+  }
 
-    @OslcDescription("Creator or creators of resource.")
-    @OslcName("creator")
-    @OslcPropertyDefinition(OslcConstants.DCTERMS_NAMESPACE + "creator")
-    @OslcRange(ArchitectureConstants.TYPE_PERSON)
-    @OslcTitle("Creators")
+  @OslcDescription("Creator or creators of resource.")
+  @OslcName("creator")
+  @OslcPropertyDefinition(OslcConstants.DCTERMS_NAMESPACE + "creator")
+  @OslcRange(ArchitectureConstants.TYPE_PERSON)
+  @OslcTitle("Creators")
     public URI[] getCreators()
     {
-        return creators.toArray(new URI[creators.size()]);
-    }
+    return creators.toArray(new URI[creators.size()]);
+  }
 
-    @OslcDescription("Descriptive text (reference: Dublin Core) about resource represented as rich text in XHTML content.")
-    @OslcPropertyDefinition(OslcConstants.DCTERMS_NAMESPACE + "description")
-    @OslcTitle("Description")
-    @OslcValueType(ValueType.XMLLiteral)
+  @OslcDescription("Descriptive text (reference: Dublin Core) about resource represented as rich text in XHTML content.")
+  @OslcPropertyDefinition(OslcConstants.DCTERMS_NAMESPACE + "description")
+  @OslcTitle("Description")
+  @OslcValueType(ValueType.XMLLiteral)
     public String getDescription()
     {
-        return description;
-    }
+    return description;
+  }
 
-    @OslcDescription("A unique identifier for a resource. Assigned by the service provider when a resource is created. Not intended for end-user display.")
-    @OslcOccurs(Occurs.ExactlyOne)
-    @OslcPropertyDefinition(OslcConstants.DCTERMS_NAMESPACE + "identifier")
-    @OslcReadOnly
-    @OslcTitle("Identifier")
+  @OslcDescription("A unique identifier for a resource. Assigned by the service provider when a resource is created. Not intended for end-user display.")
+  @OslcOccurs(Occurs.ExactlyOne)
+  @OslcPropertyDefinition(OslcConstants.DCTERMS_NAMESPACE + "identifier")
+  @OslcReadOnly
+  @OslcTitle("Identifier")
     public String getIdentifier()
     {
-        return identifier;
-    }
+    return identifier;
+  }
 
-    @OslcDescription("Resource Shape that provides hints as to resource property value-types and allowed values. ")
-    @OslcPropertyDefinition(OslcConstants.OSLC_CORE_NAMESPACE + "instanceShape")
-    @OslcRange(OslcConstants.TYPE_RESOURCE_SHAPE)
-    @OslcTitle("Instance Shape")
+  @OslcDescription("Resource Shape that provides hints as to resource property value-types and allowed values. ")
+  @OslcPropertyDefinition(OslcConstants.OSLC_CORE_NAMESPACE + "instanceShape")
+  @OslcRange(OslcConstants.TYPE_RESOURCE_SHAPE)
+  @OslcTitle("Instance Shape")
     public URI getInstanceShape()
     {
-        return instanceShape;
-    }
+    return instanceShape;
+  }
 
-    @OslcDescription("Timestamp last latest resource modification.")
-    @OslcPropertyDefinition(OslcConstants.DCTERMS_NAMESPACE + "modified")
-    @OslcReadOnly
-    @OslcTitle("Modified")
+  @OslcDescription("Timestamp last latest resource modification.")
+  @OslcPropertyDefinition(OslcConstants.DCTERMS_NAMESPACE + "modified")
+  @OslcReadOnly
+  @OslcTitle("Modified")
     public Date getModified()
     {
-        return modified;
-    }
+    return modified;
+  }
 
-    @OslcDescription("The resource type URIs.")
-    @OslcName("type")
-    @OslcPropertyDefinition(OslcConstants.RDF_NAMESPACE + "type")
-    @OslcTitle("Types")
+  @OslcDescription("The resource type URIs.")
+  @OslcName("type")
+  @OslcPropertyDefinition(OslcConstants.RDF_NAMESPACE + "type")
+  @OslcTitle("Types")
     public URI[] getRdfTypes()
     {
-        return rdfTypes.toArray(new URI[rdfTypes.size()]);
-    }
+    return rdfTypes.toArray(new URI[rdfTypes.size()]);
+  }
 
-    @OslcDescription("A short string representation for the type, example 'Defect'.")
-    @OslcName("type")
-    @OslcPropertyDefinition(OslcConstants.DCTERMS_NAMESPACE + "type")
-    @OslcTitle("DCTerms Types")
+  @OslcDescription("A short string representation for the type, example 'Defect'.")
+  @OslcName("type")
+  @OslcPropertyDefinition(OslcConstants.DCTERMS_NAMESPACE + "type")
+  @OslcTitle("DCTerms Types")
     public String[] getDctermsTypes()
     {
-        return dctermsTypes.toArray(new String[dctermsTypes.size()]);
-    }
+    return dctermsTypes.toArray(new String[dctermsTypes.size()]);
+  }
 
-    @OslcDescription("The resource URI a client can perform a get on to obtain the original non-OSLC AM formatted resource that was used to create this resource. ")
-    @OslcPropertyDefinition(OslcConstants.DCTERMS_NAMESPACE + "source")
-    @OslcTitle("Source")
+  @OslcDescription("The resource URI a client can perform a get on to obtain the original non-OSLC AM formatted resource that was used to create this resource. ")
+  @OslcPropertyDefinition(OslcConstants.DCTERMS_NAMESPACE + "source")
+  @OslcTitle("Source")
     public URI getSource()
     {
-        return source;
-    }
+    return source;
+  }
 
-    @OslcDescription("The scope of a resource is a URI for the resource's OSLC Service Provider.")
-    @OslcPropertyDefinition(OslcConstants.OSLC_CORE_NAMESPACE + "serviceProvider")
-    @OslcRange(OslcConstants.TYPE_SERVICE_PROVIDER)
-    @OslcTitle("Service Provider")
-    public URI getServiceProvider()
+  @OslcDescription("The scope of a resource is a URI for the resource's OSLC Service Provider.")
+  @OslcPropertyDefinition(OslcConstants.OSLC_CORE_NAMESPACE + "serviceProvider")
+  @OslcRange(OslcConstants.TYPE_SERVICE_PROVIDER)
+  @OslcTitle("Service Provider")
+    public Set<URI> getServiceProviders()
     {
-        return serviceProvider;
-    }
+    return serviceProviders;
+  }
 
-
-    @OslcDescription("Title (reference: Dublin Core) or often a single line summary of the resource represented as rich text in XHTML content.")
-    @OslcOccurs(Occurs.ExactlyOne)
-    @OslcPropertyDefinition(OslcConstants.DCTERMS_NAMESPACE + "title")
-    @OslcTitle("Title")
-    @OslcValueType(ValueType.XMLLiteral)
+  @OslcDescription("Title (reference: Dublin Core) or often a single line summary of the resource represented as rich text in XHTML content.")
+  @OslcOccurs(Occurs.ExactlyOne)
+  @OslcPropertyDefinition(OslcConstants.DCTERMS_NAMESPACE + "title")
+  @OslcTitle("Title")
+  @OslcValueType(ValueType.XMLLiteral)
     public String getTitle()
     {
-        return title;
-    }
+    return title;
+  }
 
 
     public void setContributors(final URI[] contributors)
     {
-        this.contributors.clear();
+    this.contributors.clear();
 
         if (contributors != null)
         {
-            this.contributors.addAll(Arrays.asList(contributors));
-        }
+      this.contributors.addAll(Arrays.asList(contributors));
     }
+  }
 
     public void setCreated(final Date created)
     {
-        this.created = created;
-    }
+    this.created = created;
+  }
 
     public void setCreators(final URI[] creators)
     {
-        this.creators.clear();
+    this.creators.clear();
 
         if (creators != null)
         {
-            this.creators.addAll(Arrays.asList(creators));
-        }
+      this.creators.addAll(Arrays.asList(creators));
     }
+  }
 
     public void setDescription(final String description)
     {
-        this.description = description;
-    }
+    this.description = description;
+  }
 
     public void setIdentifier(final String identifier)
     {
-        this.identifier = identifier;
-    }
+    this.identifier = identifier;
+  }
 
     public void setInstanceShape(final URI instanceShape)
     {
-        this.instanceShape = instanceShape;
-    }
+    this.instanceShape = instanceShape;
+  }
 
     public void setModified(final Date modified)
     {
-        this.modified = modified;
-    }
+    this.modified = modified;
+  }
 
     public void setRdfTypes(final URI[] rdfTypes)
     {
-        this.rdfTypes.clear();
+    this.rdfTypes.clear();
 
         if (rdfTypes != null)
         {
-            this.rdfTypes.addAll(Arrays.asList(rdfTypes));
-        }
+      this.rdfTypes.addAll(Arrays.asList(rdfTypes));
     }
+  }
 
     public void setDctermsTypes(final String[] dctermsTypes)
     {
-        this.dctermsTypes.clear();
+    this.dctermsTypes.clear();
 
         if (dctermsTypes != null)
         {
-            this.dctermsTypes.addAll(Arrays.asList(dctermsTypes));
-        }
+      this.dctermsTypes.addAll(Arrays.asList(dctermsTypes));
     }
+  }
 
     public void setSource(final URI source)
     {
-        this.source = source;
+    this.source = source;
+  }
+
+    public void setServiceProvider(final URI[] serviceProviders)
+    {
+    this.serviceProviders.clear();
+        if (serviceProviders != null)
+        {
+      this.serviceProviders.addAll(Arrays.asList(serviceProviders));          
     }
 
-    public void setServiceProvider(final URI serviceProvider)
-    {
-        this.serviceProvider = serviceProvider;
-    }
+  }
 
 
     public void setTitle(final String title)
     {
-        this.title = title;
-    }
+    this.title = title;
+  }
 
 }
