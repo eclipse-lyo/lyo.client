6aae1938103ea61e62236bddd0fae67c89770a0a
Jim Amsden <jamsden@us.ibm.com>
2019-02-07 14:15:54 -0500

    Renamed again to be more consistent with core's use of oslc4j

--- oslc4j-client-samples/src/main/java/org/eclipse/lyo/client/oslc/samples/RMSample.java
+++ oslc4j-client-samples/src/main/java/org/eclipse/lyo/client/oslc/samples/RMSample.java
@@ -23,14 +23,15 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import org.apache.http.HttpStatus;
+import org.eclipse.lyo.oslc4j.client.OSLCConstants;
+import org.eclipse.lyo.oslc4j.client.OslcClient;
+import org.eclipse.lyo.oslc4j.client.resources.OslcQuery;
+import org.eclipse.lyo.oslc4j.client.resources.OslcQueryParameters;
+import org.eclipse.lyo.oslc4j.client.resources.OslcQueryResult;
+import org.eclipse.lyo.oslc4j.client.resources.Requirement;
+
 import javax.ws.rs.core.Response;
 
-import org.eclipse.lyo.client.OSLCConstants;
-import org.eclipse.lyo.client.OslcClient;
-import org.eclipse.lyo.client.resources.OslcQuery;
-import org.eclipse.lyo.client.resources.OslcQueryParameters;
-import org.eclipse.lyo.client.resources.OslcQueryResult;
-import org.eclipse.lyo.client.resources.Requirement;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.GnuParser;
 import org.apache.commons.cli.Options;
